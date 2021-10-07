ad118f05ffeb804448f0d979d37f6bb56afd80c6
Vikas Maury <mauryvikas12345@gmail.com>
2021-10-07 23:58:31 +0530

    Second Funny commit
    This is just a dummy project to test if everything is working fine or not.

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textview = findViewById<TextView>(R.id.ele1)
+        val imageview = findViewById<ImageView>(R.id.backImage)
+        textview.setOnClickListener {
+            textview.setText(" Please don't touch \nhere ")
+            imageview.setImageResource(R.drawable.rakool)
+            textview.setOnClickListener {
+                val toast = Toast.makeText(this,"Please don't do this.",Toast.LENGTH_SHORT)
+                toast.show()
+            }
+        }
     }
 }
